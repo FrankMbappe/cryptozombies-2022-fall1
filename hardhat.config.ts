7fbe930a0890a2a946e74104c4aa3c5a10726c16
Frank Mbappe <kevyndrews@gmail.com>
2022-11-29 11:22:59 +0100

    ✨ Deploy smart contract on Goerli Update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+// Environment variables
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
-  defaultNetwork: "ganache",
+  defaultNetwork: "goerli",
   networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY!],
+    },
     ganache: {
       url: "http://127.0.0.1:7545",
       accounts: [
-        "be88d2abc8e54091dc1f0e06d2e90945abe6442430fa7f75e4bab7a5c1dde346",
+        "a4a11159ecdb8598f9c66f5ebee3c952147843cec3a59352e92778d1a517faf2",
       ],
       chainId: 1337,
     },
